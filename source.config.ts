80ad619aa551784a6d7b5a4166886552785af429
Ján Kovačovský <kovacoj1@gmail.com>
2026-08-02 15:35:10 +0200

    docs: render article math and citations

--- source.config.ts
+++ source.config.ts
@@ -1,8 +1,11 @@
 import { remarkMarimo } from '@marimo-team/mdx-marimo/remark';
 import { defineConfig } from 'fumadocs-mdx/config';
+import rehypeKatex from 'rehype-katex';
+import remarkMath from 'remark-math';
 
 export default defineConfig({
   mdxOptions: {
-    remarkPlugins: (plugins) => [remarkMarimo, ...plugins],
+    remarkPlugins: (plugins) => [remarkMath, remarkMarimo, ...plugins],
+    rehypePlugins: (plugins) => [rehypeKatex, ...plugins],
   },
 });
